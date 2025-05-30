72e09d383fafb3de7ef1ba50fc0f057a4d88d03a
Omotayo <179114835+1827765@users.noreply.github.com>
2025-05-30 03:18:55 -0500

    refactor: update layout and header components, update dependencies
    - Modified layout.tsx and page.tsx for improved structure
    - Updated Header component for better navigation
    - Updated package.json and package-lock.json dependencies
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/layout.tsx
    	modified:   src/app/page.tsx
    	modified:   src/components/Header.tsx
    	src/app/constants/
    	src/app/shop/
    	src/components/Container.tsx
    	src/components/Footer.tsx
    	src/components/Logo.tsx
    	src/components/MobileMenu.tsx
    	src/components/NavMenu.tsx
    	src/components/Searchbar.tsx
    	src/components/SideMenu.tsx
    	src/components/Signin.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
+import Container from "@/components/Container";
 
 export default function Home() {
-  return <Header />;
+  return <Container></Container>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { Button } from "./ui/button";
+'"use client";';
+import Container from "./Container";
+import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
+import NavMenu from "./NavMenu";
+import Searchbar from "./Searchbar";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-5 flex justify-between items-center h-16 w-full">
-      <div className="font-bold text-2xl bg-gradient-to-r from-green-900 to-green-300 bg-clip-text text-transparent">
-        Shop-cart
-      </div>
-      <div className="flex items-center relative">
-        {["home", "Shop", "Blog", "Contact", "Hot Deal"].map((item, index) => (
-          <a
-            key={index}
-            href={`#${item.toLowerCase()}`}
-            className="mx-4 hover:text-green-300 transition-colors"
-          >
-            {item}
-            <span className="block h-0.5 w-0 bg-green-400 transition-all duration-300 origin-right hover:w-full absolute bottom-0"></span>
-          </a>
-        ))}
-      </div>
-      <div></div>
+    <header className=" text-white shadow-md sticky top-0 z-50">
+      <Container className=" text-black p-5 flex justify-between items-center h-16 max-w-full">
+        <div className="flex items-center gap-2.5 w-auto md:w-1/3 justify-start md:gap-0">
+          <MobileMenu /> <Logo />
+        </div>
+
+        <NavMenu />
+        {/*Search bar*/}
+        {/*admin navbar*/}
+        <Searchbar />
+      </Container>
     </header>
   );
 };
